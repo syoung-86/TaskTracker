c1bc7100db0fcc87e6a114ecdd0ef81aa6623887
syoung-86 <25084409+syoung-86@users.noreply.github.com>
2024-08-04 14:58:40 +0200

    created tasks app and added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './tasks/templates/**/*.html',
+    // Add other paths where you have HTML files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
